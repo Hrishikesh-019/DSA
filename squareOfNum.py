0f00975de0ee9a212b54bcd85145d55cadc29a4a
Hrishikesh <hrishikesh1901@gmail.com>
2025-12-16 16:20:38 +0530

    day 2 -- 3 tasks completed

---
+++ squareOfNum.py
@@ -0,0 +1,34 @@
+# ##Square Of Number
+# def SquarOfNum(a):
+#     return a**a
+# res=SquarOfNum(5)
+# print(res)
+
+#return
+
+# Sends value back
+
+# Can be reused
+
+# Used in interviews & real projects
+
+#FUNCTIONAL ARGUMENTS
+
+# Positional Argumets
+# def greet(name, city):
+#     print (name, city)
+# greet("Rishi,", "Hyderabad")
+
+# Default Argument
+#def greet(name, city="Hyderabad"):
+#     print(name,city)
+# greet("Rishi")
+
+# *args Multiple Arguments
+
+def multiples(*numbers):
+    total=1
+    for n in numbers:
+        total*=n
+    return total
+print(multiples(1,2,3,4,5))
